badb44db5e36c7d8e2f94b3b321061fcb15c2d4f
TurnerMay <turnerwmay@gmail.com>
2022-01-25 16:49:55 -0600

    Stopping Point

---
+++ app.js
@@ -0,0 +1,36 @@
+let Myname = "Turner May";
+
+const USstates = 50;
+
+var Step6 = 5 + 4;
+
+function sayHello() {
+  alert("Hello World!");
+}
+sayHello();
+
+
+function checkAge(name, age) {
+    if(age < 21) {alert("Sorry " + name + ", you aren't old enough to view this page!")
+
+    }
+}
+
+checkAge("Charles", 22)
+checkAge("Abby", 27)
+checkAge("James", 18)
+checkAge("John", 17)
+
+var vegetables = ['Asparagus', 'Onion', 'Brussel Sprouts', 'Squash', 'Zucchini', ]
+
+for(var i = 0; i < vegetables.length; i++){
+    console.log(vegetables[i]);
+}
+
+var Pet = {
+    name: 'Meko',
+    breed: 'Boxer',
+    
+}
+
+{console.log(Pet)}
