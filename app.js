8f223f71a7e91c6bd5f4bc57969e1a4e8a518863
TurnerMay <turnerwmay@gmail.com>
2022-01-26 11:10:31 -0600

    Finished Kitchen Sink

--- app.js
+++ app.js
@@ -9,28 +9,71 @@ function sayHello() {
 }
 sayHello();
 
-
 function checkAge(name, age) {
-    if(age < 21) {alert("Sorry " + name + ", you aren't old enough to view this page!")
-
-    }
+  if (age < 21) {
+    alert("Sorry " + name + ", you aren't old enough to view this page!");
+  }
 }
 
-checkAge("Charles", 22)
-checkAge("Abby", 27)
-checkAge("James", 18)
-checkAge("John", 17)
+checkAge("Charles", 22);
+checkAge("Abby", 27);
+checkAge("James", 18);
+checkAge("John", 17);
 
-var vegetables = ['Asparagus', 'Onion', 'Brussel Sprouts', 'Squash', 'Zucchini', ]
+var vegetables = [
+  "Asparagus",
+  "Onion",
+  "Brussel Sprouts",
+  "Squash",
+  "Zucchini",
+];
 
-for(var i = 0; i < vegetables.length; i++){
-    console.log(vegetables[i]);
+for (var i = 0; i < vegetables.length; i++) {
+  console.log(vegetables[i]);
 }
 
 var Pet = {
-    name: 'Meko',
-    breed: 'Boxer',
-    
+  name: "Meko",
+  breed: "Boxer",
+};
+
+console.log(Pet.name, Pet.breed);
+
+var Friends = [
+  {
+    Name: "Will",
+    Age: 25,
+  },
+  {
+    Name: "Jimmy",
+    Age: 18,
+  },
+  {
+    Name: "Jordan",
+    Age: 32,
+  },
+  {
+    Name: "Randy",
+    Age: 33,
+  },
+  {
+    Name: "Craig",
+    Age: 59,
+  },
+];
+
+for (var i = 0; i < Friends.length; i++) {
+  checkAge(Friends[i].Name, Friends[i].Age);
 }
 
-{console.log(Pet)}
+function getLength(string) {
+  return string.length;
+}
+
+let stringLength = getLength("hello world");
+
+if (stringLength % 2 == 0) {
+  console.log("The world is nice and even!");
+} else {
+  console.log("The world is an odd place!");
+}
